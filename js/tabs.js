8063ba47613d8dfd88d965dee7982984e350563c
Olya308 <121465155+Olha21-new@users.noreply.github.com>
2024-04-22 22:31:37 +0300

    Add files via upload

--- js/tabs.js
+++ js/tabs.js
@@ -1,24 +1,18 @@
-"use strict";
-
-export function clickTab(tab, tabContents) {
-  tab.addEventListener("click", () => {
-    const target = document.querySelector(tab.dataset.tabTarget);
-    tabContents.forEach((tabContent) => {
-      tabContent.classList.remove("active");
-    });
-    tabs.forEach((tab) => {
-      tab.classList.remove("active");
-    });
-    target.classList.add("active");
-    tab.classList.add("active");
-  });
-}
-
 export function initializeTabs() {
   const tabs = document.querySelectorAll("[data-tab-target]");
   const tabContents = document.querySelectorAll("[data-tab-content]");
 
   tabs.forEach((tab) => {
-    clickTab(tab, tabContents);
+    tab.addEventListener("click", () => {
+      const target = document.querySelector(tab.dataset.tabTarget);
+      tabContents.forEach((tabContent) => {
+        tabContent.classList.remove("active");
+      });
+      tabs.forEach((tab) => {
+        tab.classList.remove("active");
+      });
+      target.classList.add("active");
+      tab.classList.add("active");
+    });
   });
 }
